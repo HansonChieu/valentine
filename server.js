626afc1a90250039a3be76c867216234ee90c2cf
Hanson Chieu <10275180+HansonChieu@users.noreply.github.com>
2025-02-03 16:56:14 +0000

    Updated and added required files

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const path = require("path");
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "home.html"));
+});
+
+app.get("/answer", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "answer.html"));
+});
+
+app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
